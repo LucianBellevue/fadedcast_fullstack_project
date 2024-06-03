9cc7e796d067ba67af072035d00a52fff8dc0dd6
Diego Medina <romeomdna@outlook.com>
2024-06-02 22:22:46 -0400

    adding components for layout and sections

---
+++ components/PodcastCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PodcastCard = ({imgURL, title, description, podcastId}
+    : {
+      imgURL: string,
+      title: string,
+      description: string,
+      podcastId: number,
+    }
+) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PodcastCard
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className='right_sidebar'></section>
+  )
+}
+
+export default RightSidebar
