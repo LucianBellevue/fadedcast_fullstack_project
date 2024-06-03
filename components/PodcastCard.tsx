72a09d6c7776ff0b5060200f3114e40b56d247ae
Diego Medina <romeomdna@outlook.com>
2024-06-02 23:02:30 -0400

    convex plug in and installment

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const PodcastCard = ({imgURL, title, description, podcastId}
     : {
@@ -9,7 +10,14 @@ const PodcastCard = ({imgURL, title, description, podcastId}
     }
 ) => {
   return (
-    <div>
+    <div className='cursor-point'>
+        <figure className='flex flex-col gap-2'>
+            <Image src={imgURL} alt={title} width={174} height={174} className="aspect-square h-fit w-full rounded-xl 2xl:size-[200px]" />
+            <div className='flex flex-col'>
+                <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+                <h2 className='text-12 truncate text-white-1'>{description}</h2>
+            </div>
+        </figure>
 
     </div>
   )
